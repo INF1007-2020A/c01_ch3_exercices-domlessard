a1525000be356530ce69cd13d3b0a868a3d1b16b
domlessard <dominik.lessard@polymtl.ca>
2020-09-14 18:29:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 def to_degrees(angle_rads: float) -> tuple:
     deg = 1 
     minutes = 1/60
-    seconds = 1/3600
+    seconds = 
 
     return 
 
